0ecd94742251885ff5a239134127688305506555
Lance Vincent Bumanglag <lancevincent@gmail.com>
2015-08-13 02:24:46 +0800

    added index for pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,32 @@
 class PinsController < ApplicationController
+	before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
+	before_action :authenticate_user!, except: [:index, :show]
+
+	def index
+		@pins = Pin.all.order('created_at DESC')
+	end
+
+	def show
+		
+	end
+
+	def new
+		@pin = current_user.pins.new
+	end
+
+	def create
+		
+	end
+
+
+	private
+
+	def find_pin
+		@pin = Pin.find(params[:id])
+	end
+
+	def pin_params
+		params.require(:pin).permit(:title, :description, :image)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :pins
+
+  root 'pins#index'
 end
